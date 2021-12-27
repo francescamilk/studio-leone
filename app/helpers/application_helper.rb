0e7dc555679c40ed4597d8f6bfc96edd5d50f60c
Francesca Santoriello <f.santoriello@hotmail.com>
2021-12-27 17:30:35 +0100

    admin dash css

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,5 +1,9 @@
 module ApplicationHelper
-  def admin_form?
+  def admin_dash? 
+    controller_name == 'pages' && action_name == 'dashboard'
+  end
+
+  def glass_form?
     controller_name == 'glasses' && action_name == 'new' || action_name == 'edit'
   end
 end
