8ad7534373e90680670ad140946ad45961d27bf4
Francesca Santoriello <f.santoriello@hotmail.com>
2022-01-03 15:42:43 +0100

    show design draft

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -6,4 +6,20 @@ module ApplicationHelper
   def glass_form?
     controller_name == 'glasses' && action_name == 'new' || action_name == 'edit'
   end
+
+  def glass_show?
+    controller_name == 'glasses' && action_name == 'show'
+  end
+
+  def no_navbar?
+    admin_dash? || glass_form? || glass_show?
+  end
+
+  def no_footer?
+    admin_dash? || glass_form? || glass_show?
+  end
+
+  def no_background?
+    glass_show?
+  end
 end
