a285c22b8ec7a12e3f1becb650645fcba3c9f200
Francesca Santoriello <f.santoriello@hotmail.com>
2022-01-26 14:29:16 +0100

    catalog/seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -20,28 +20,25 @@ end
 puts 'Clearing db...'
 Glass.destroy_all
 
-# ARMANI
-puts 'Loading Armani catalog...'
-seed_csv("armani")
+# # ARMANI
+# puts 'Loading Armani catalog...'
+# seed_csv("armani")
 
-# MIU MIU 
-puts 'Loading Miu Miu catalog...'
-seed_csv("miumiu")
-
-# PERSOL
-puts 'Loading Persol catalog...'
+# # MIU MIU 
+# puts 'Loading Miu Miu catalog...'
+# seed_csv("miumiu")
 
 # VOGUE
 puts 'Loading Vogue catalog...'
 seed_csv("vogue")
 
-puts 'All done!'
-
-# temporary images
+puts 'Loading images...'
 Glass.all.each do |glass|
-  glass.photos.attach(io: File.open("./01_01.png"), filename: "01_01.png", content_type: "image/png")
-  glass.photos.attach(io: File.open("./01_02.png"), filename: "01_02.png", content_type: "image/png")
-  glass.photos.attach(io: File.open("./01_03.png"), filename: "01_03.png", content_type: "image/png")
-  glass.photos.attach(io: File.open("./01_04.png"), filename: "01_04.png", content_type: "image/png")
-  glass.photos.attach(io: File.open("./01_05.png"), filename: "01_05.png", content_type: "image/png")
+  glass.photos.attach(io: File.open("./app/assets/images/#{glass.brand.downcase}/#{glass.sku}_01.png"), filename: "#{glass.sku}_01.png", content_type: "image/png")
+  glass.photos.attach(io: File.open("./app/assets/images/#{glass.brand.downcase}/#{glass.sku}_02.png"), filename: "#{glass.sku}_02.png", content_type: "image/png")
+  glass.photos.attach(io: File.open("./app/assets/images/#{glass.brand.downcase}/#{glass.sku}_03.png"), filename: "#{glass.sku}_03.png", content_type: "image/png")
+  glass.photos.attach(io: File.open("./app/assets/images/#{glass.brand.downcase}/#{glass.sku}_04.png"), filename: "#{glass.sku}_04.png", content_type: "image/png")
+  glass.photos.attach(io: File.open("./app/assets/images/#{glass.brand.downcase}/#{glass.sku}_05.png"), filename: "#{glass.sku}_05.png", content_type: "image/png")
 end
+
+puts 'All done!'
